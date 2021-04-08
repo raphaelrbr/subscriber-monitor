5774d7b665e26c144c7f6cb0d66cb42de518518f
Raphael Ribeiro <raphaelrbr@usp.br>
2021-04-08 12:17:14 -0300

    Expose Websocket endpoint

--- main.go
+++ main.go
@@ -4,14 +4,30 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/raphaelrbr/subscriber-monitor/websocket"
 )
 
 func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Youtube Monitor!")
 }
 
+// Expose YouTube stats via a websocket connection
+func stats(w http.ResponseWriter, r *http.Request) {
+
+	// upgrade HTTP connection to a webscocket one
+	ws, err := websocket.Upgrade(w, r)
+	if err != nil {
+		fmt.Fprintf(w, "%+v\n", err)
+	}
+
+	// Call Writter function to this websocket connection
+	go websocket.Writer(ws)
+}
+
 func setupRoutes() {
 	http.HandleFunc("/", homePage)
+	http.HandleFunc("/stats", stats)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
