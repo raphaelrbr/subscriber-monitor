be6969dc0a96926d54d48c9c50ade299b62a2f62
Raphael Ribeiro <raphaelrbr@usp.br>
2021-04-08 12:13:32 -0300

    set up Websocket Endpoint

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/raphaelrbr/subscriber-monitor
 
 go 1.16
+
+require github.com/gorilla/websocket v1.4.2
---
+++ websocket/websocket.go
@@ -0,0 +1,62 @@
+package websocket
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/websocket"
+	"github.com/raphaelrbr/subscriber-monitor/youtube"
+)
+
+// Set up Read and Write buffers
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+// Receive an incoming request and upgrade the request into
+// a websocket connection
+func Upgrade(w http.ResponseWriter, r *http.Request) (*websocket.Conn, error) {
+	// Alow other origin hosts connect to our websocket server
+	upgrader.CheckOrigin = func(r *http.Request) bool { return true }
+
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+		return ws, err
+	}
+
+	return ws, nil
+}
+
+// websocket.go
+func Writer(conn *websocket.Conn) {
+
+	for {
+		ticker := time.NewTicker((10 * time.Second))
+
+		for t := range ticker.C {
+			fmt.Printf("Update: %+v\n", t)
+			items, err := youtube.GetSubscribers()
+			if err != nil {
+				fmt.Println(err)
+			}
+			//marshal our response into a JSON string
+			jsonString, err := json.Marshal(items)
+			if err != nil {
+				fmt.Println(err)
+			}
+			// write this JSON string to our WebSocket
+			// connection and record any errors
+			err = conn.WriteMessage(websocket.TextMessage, []byte(jsonString))
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+
+		}
+	}
+}
