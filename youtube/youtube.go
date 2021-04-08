3671be5929e09351f0379625629e9bdaaf26dd8b
Raphael Ribeiro <raphaelrbr@usp.br>
2021-04-08 11:28:52 -0300

    implement GetSubscribers

---
+++ youtube/youtube.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+)
+
+type Response struct {
+	Kind  string `json:"kind"`
+	Items []Item `json:"items"`
+}
+
+type Item struct {
+	Kind  string `json:"kind"`
+	Id    string `json:"id"`
+	Stats Stats  `json:"statistics"`
+}
+
+type Stats struct {
+	Views       string `json:"viewCount"`
+	Subscribers string `json:"videoCount"`
+	Videos      string `json:"videoCount"`
+}
+
+func GetSubscribers() (Item, error) {
+	var response Response
+	req, err := http.NewRequest("GET", "https://www.googleapis.com/youtube/v3/channels", nil)
+	if err != nil {
+		fmt.Println(err)
+		return Item{}, err
+	}
+
+	// Query parameters
+	q := req.URL.Query()
+
+	q.Add("key", os.Getenv("YOUTUBE_KEY")) // Environment Variable
+	q.Add("id", os.Getenv("CHANNEL_ID"))   // Environment Variable
+	q.Add("part", "statistics")
+	req.URL.RawQuery = q.Encode()
+
+	// Request
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return Item{}, err
+	}
+	defer resp.Body.Close()
+
+	fmt.Println("Response Status: ", resp.Status)
+
+	// read the JSON response
+	body, _ := ioutil.ReadAll(resp.Body)
+	// Umarshal into an Response Struct
+	err = json.Unmarshal(body, &response)
+	if err != nil {
+		return Item{}, err
+	}
+
+	return response.Items[0], nil
+}
